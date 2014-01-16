ba311fceb20da94e70bdb072ccdbeab773973d26
Wolfram Gulde <wolfram.gulde@exxcellent.de>
2014-01-16 18:00:17 +0100

    Carousel in MatchMain

--- app/view/MatchMainView.js
+++ app/view/MatchMainView.js
@@ -1,11 +1,22 @@
 Ext.define('Epta.view.MatchMainView', {
 	extend: 'Ext.form.Panel',
     xtype: 'matchmainview',
-    requires: ['Ext.field.Toggle'],
+    requires: ['Ext.field.Toggle',
+               'Ext.carousel.Carousel'
+    ],
 
     config: {
     	title: 'MatchMainView',
 	    styleHtmlContent: true,
+	    cls: 'cards',
+	    layout: {
+    		type : 'vbox',
+    		align: 'stretch'
+    	},
+    	defaults: {
+    		flex: 1
+		},   	
+    
 	    	    
 	    items: [{
         	docked: 'top',
@@ -25,9 +36,25 @@ Ext.define('Epta.view.MatchMainView', {
 			       align: 'right'
 		    	 }			     
 	        ]
-        }],           
-        
-        html: 'GGAME'
+        }, {
+        	xtype: 'carousel',
+        	activeItem: 1,
+        	items : [
+        	    {
+        	    	html: '<p>left</p>',
+        	    	cls: 'card'
+    			},
+    			{
+        	    	html: '<p>middle</p>',
+        	    	cls: 'card'
+    			},
+    			{
+        	    	html: '<p>right</p>',
+        	    	cls: 'card'
+    			}
+        	
+	         ]
+    	}]
 	}
 	
 });
